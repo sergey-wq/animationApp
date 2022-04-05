48ae22aa7d1f9dae7ad9797f53630fe3c0d55459
sergeykin <sergeykin1997@mail.ru>
2022-04-05 19:47:22 +0300

    feat: added app settings

---
+++ AnimationAppTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationAppTest
+//
+//  Created by Sergey Runovich on 5.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
